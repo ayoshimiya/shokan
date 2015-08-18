5bb0c9ea51dc4b7f6e8f4ddc9ea37fda7c2c5a00
ayoshimiya <ayoshimiya@gmail.com>
2015-08-17 22:47:54 -0300

    adds books scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150817225729) do
+ActiveRecord::Schema.define(version: 20150818013803) do
+
+  create_table "books", force: :cascade do |t|
+    t.string   "isbn"
+    t.string   "name"
+    t.integer  "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
